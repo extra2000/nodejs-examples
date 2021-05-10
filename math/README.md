294e11b61d196b3a459b382b53c2b26acaabd9d8
Nik Mohamad Aizuddin b. Nik Azmi <nik-mohamad-aizuddin@yandex.com>
2021-05-11 00:28:34 +0800

    feat(example): Add `math` example

---
+++ math/README.md
@@ -0,0 +1,6 @@
+# Sample apps: Math
+
+```
+$ podman build -t extra2000/mathapp:latest .
+$ podman run --rm extra2000/mathapp
+```
---
+++ math/project/src/main.ts
@@ -0,0 +1,7 @@
+function add(x: number, y: number): number {
+    var result: number = x + y;
+    return result;
+}
+
+const result: number = add(10, 20);
+console.log(`result = ${result}`);
